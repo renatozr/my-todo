7457509b6f3e8a50bedf1fb13817d3973d034dde
Renato Zanella Redoglia <renatozr07@gmail.com>
2023-01-29 17:13:02 -0300

    prod server setup

--- client/src/components/Loading.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Loading() {
-  return (
-    <div>
-      <span className="button is-loading is-size-1" style={{ border: 0 }} />
-    </div>
-  );
-}
