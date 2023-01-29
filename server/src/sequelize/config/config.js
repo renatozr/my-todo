1d2af89ec9b60bf350fd179a6bfddc49906a9cd6
Renato Zanella Redoglia <renatozr07@gmail.com>
2023-01-29 17:46:29 -0300

    change sequelize prod config

--- server/src/sequelize/config/config.js
+++ server/src/sequelize/config/config.js
@@ -17,7 +17,11 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    url: process.env.DATABASE_URL,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'postgres',
   },
 };
