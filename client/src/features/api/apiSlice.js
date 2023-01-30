e1f9ee29d68e6d34e1fa06518f01a95b1b123cb2
Renato Zanella Redoglia <renatozr07@gmail.com>
2023-01-30 00:50:26 -0300

    api url

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.MYTODO_API_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://my-todo-api-szq3.onrender.com' }),
   tagTypes: ['Task'],
   endpoints: (builder) => ({
     getTasks: builder.query({
