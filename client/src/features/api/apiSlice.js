399c27ac2a5a823f2911951f96948e831f722d64
Renato Zanella Redoglia <renatozr07@gmail.com>
2023-01-30 00:47:05 -0300

    change api url env name

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.API_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.MYTODO_API_URL }),
   tagTypes: ['Task'],
   endpoints: (builder) => ({
     getTasks: builder.query({
