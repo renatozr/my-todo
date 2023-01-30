84e833b606c3df6771e9488849657d317b13046d
Renato Zanella Redoglia <renatozr07@gmail.com>
2023-01-30 00:58:39 -0300

    fix api url env var

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://my-todo-api-szq3.onrender.com' }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_MYTODO_API_URL }),
   tagTypes: ['Task'],
   endpoints: (builder) => ({
     getTasks: builder.query({
